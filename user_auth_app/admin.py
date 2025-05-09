acc786156c72009f16c6beb15f7e5891dc1df6a0
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-05-09 10:45:15 +0200

    added profile to admin site

--- user_auth_app/admin.py
+++ user_auth_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from rest_framework.authtoken.models import Token
+
+from user_auth_app.models import Profile
 
 # Register your models here.
+admin.site.register(Profile)
