227c3b76e448a891f56a3dec33d135d05ebb7276
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-05-11 14:58:19 +0200

    edited board create

--- board_app/api/serializers.py
+++ board_app/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
+from rest_framework.response import Response
 
-class BoardSerializer(serializers.Serializer):
+from board_app.admin import Board
+from user_auth_app.models import Profile
+
+
+class BoardReadSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField()
     member_count = serializers.IntegerField()
@@ -8,3 +13,31 @@ class BoardSerializer(serializers.Serializer):
     tasks_to_do_count = serializers.IntegerField()
     tasks_high_prio_count = serializers.IntegerField()
     owner_id = serializers.IntegerField()
+
+
+class BoardWriteSerializer(serializers.ModelSerializer):
+    members = serializers.PrimaryKeyRelatedField(
+        queryset=Profile.objects.all(),
+        many=True,
+        error_messages={
+            "does_not_exist": "Member with ID {pk_value} does not exist!"
+        }
+    )
+
+    class Meta:
+        model = Board
+        fields = ['title', 'members']
+
+    def create(self, validated_data):
+        owner = validated_data.pop('owner', None)
+        members = validated_data.pop('members', [])
+        filtered_members = [
+            member for member in members if member.id != owner.id]
+        try:
+            board = Board.objects.create(owner_id=owner, **validated_data)
+            board.members.set(filtered_members)
+            return board
+        except Exception as e:
+            return Response({
+                "error": "Internal Server error!"
+            })
--- board_app/api/views.py
+++ board_app/api/views.py
@@ -1,18 +1,37 @@
 from django.db import models
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework import generics, status
+from rest_framework import permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import AuthenticationFailed, MethodNotAllowed
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from board_app.api.permissions import BoardOwnerOrMemberAuthentication
-from board_app.api.serializers import BoardSerializer
+from board_app.api.serializers import BoardReadSerializer, BoardWriteSerializer
 from board_app.models import Board
 from task_app.models import Task
 from user_auth_app.models import Profile
 
 
 class BoardListView(generics.ListAPIView):
-    serializer_class = BoardSerializer
-    permission_classes = [BoardOwnerOrMemberAuthentication]
+    def get_permissions(self):
+        if self.request.method == "GET":
+            permission_classes = [BoardOwnerOrMemberAuthentication]
+        elif self.request.method == "POST":
+            permission_classes = [IsAuthenticated]
+        else:
+            raise MethodNotAllowed(self.request.method,
+                                   detail="Only Get and Post are supported")
+        return [permission() for permission in permission_classes]
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return BoardReadSerializer
+        elif self.request.method == "POST":
+            return BoardWriteSerializer
+        else:
+            raise MethodNotAllowed(self.request.method,
+                                   detail="Only GET and POST are supported")
 
     def get(self, request):
         user = request.user
@@ -21,14 +40,12 @@ class BoardListView(generics.ListAPIView):
             models.Q(owner_id=profile) | models.Q(members=profile)
         ).distinct()
 
-        for board in boards:
-            data = self.setBoardView(board)
+        data = [self.set_board_view(board) for board in boards]
         return Response(data)
 
-    def setBoardView(self, board):
+    def set_board_view(self, board):
         tasks = Task.objects.filter(board=board)
-        data = []
-        data.append({
+        return {
             "id": board.id,
             "title": board.title,
             "member_count": board.members.count(),
@@ -36,5 +53,29 @@ class BoardListView(generics.ListAPIView):
             "tasks_to_do_count": tasks.filter(status="to-do").count(),
             "tasks_high_prio_count": tasks.filter(priority="high").count(),
             "owner_id": board.owner_id.id,
-        })
-        return data
+        }
+
+    def post(self, request):
+        serializer = self.get_serializer(
+            data=request.data, context={'request': request})
+        user_id = self.get_user_from_token(request)
+        profile = Profile.objects.filter(user_id=user_id).first()
+        if not profile:
+            return Response({"error": "No profile found for the user."}, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            board = serializer.save(owner=profile)
+            board_data = self.set_board_view(board)
+            return Response(board_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_user_from_token(self, request):
+        auth_header = request.headers.get('Authorization')
+        if not auth_header or not auth_header.startswith('Token '):
+            raise AuthenticationFailed("No valid Token found!")
+
+        token_key = auth_header.split('Token ')[1]
+        try:
+            token = Token.objects.get(key=token_key)
+            return token.user_id
+        except Token.DoesNotExist:
+            raise AuthenticationFailed("Invalid Token!")
