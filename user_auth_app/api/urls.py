0e65970576e5f9f177445e4a044c5cdd6da75b8e
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-05-12 13:11:12 +0200

    email checkview was added

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from user_auth_app.api.views import ProfilLoginView, ProfilRegistrationView
+from user_auth_app.api.views import EmailCheckView, ProfilLoginView, ProfilRegistrationView
 
 
 urlpatterns = [
     path('registration/', ProfilRegistrationView.as_view(), name="registration"),
     path('login/', ProfilLoginView.as_view(), name="login"),
+    path('email-check/', EmailCheckView.as_view(), name="email-check"),
 ]
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.core.validators import validate_email
 
 from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from user_auth_app.api.serializers import LoginSerializer, ProfilRegistrationSerializer
+from user_auth_app.api.serializers import LoginSerializer, MemberSerializer, ProfilRegistrationSerializer
 from user_auth_app.models import Profile
 
 
@@ -42,15 +44,16 @@ class ProfilRegistrationView(generics.CreateAPIView):
             user=user,
             fullname=validated_data["fullname"]
         )
-    
+
+
 class ProfilLoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
     permission_classes = [AllowAny]
 
     def authenticateUser(self, serializer):
         return authenticate(
-            username = serializer.validated_data["email"],
-            password = serializer.validated_data["password"]
+            username=serializer.validated_data["email"],
+            password=serializer.validated_data["password"]
         )
 
     def post(self, request, *args, **kwargs):
@@ -69,11 +72,51 @@ class ProfilLoginView(generics.GenericAPIView):
                     "email": user.email,
                     "user_id": user.id
                 }, status=status.HTTP_200_OK)
-            
+
             return Response({
                 "error": "Invalid email or password"
             }, status=status.HTTP_400_BAD_REQUEST)
-        
+
         except Exception as e:
             return Response({"error": f"An internal server error occurred. {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
+class EmailCheckView(APIView):
+    serializer_class = MemberSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        email = request.query_params.get('email')
+
+        if not email:
+            return Response({
+                'error': 'Email address is missing'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            validate_email(email)
+        except Exception:
+            return Response({
+                'error': 'Wrong email format!'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(email=email)
+            profile = Profile.objects.get(user=user)
+            return Response({
+                "id": profile.id,
+                'email': user.email,
+                'fullname': profile.fullname
+            })
+        except User.DoesNotExist:
+            return Response({
+                'error': 'User do not exist!'
+            })
+        except Profile.DoesNotExist:
+            return Response({
+                'error': 'Profile do not exist!'
+            })
+        except Exception as e:
+            return Response({
+                'error': 'An internal server error occurred.!'
+            })
